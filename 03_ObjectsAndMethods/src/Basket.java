9c82b5d497de682cc471a290ff2b9535eb23c9f6
@ivan_okhlopkov <iokhlopkov@gmail.com>
2021-08-04 15:16:18 +0300

    add total weight

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -4,6 +4,8 @@ public class Basket {
     private String items = "";
     private int totalPrice = 0;
     private int limit;
+    private double totalWeight = 0;
+    public static double weight = 0;
 
     public Basket() {
         increaseCount(1);
@@ -31,10 +33,12 @@ public class Basket {
     }
 
     public void add(String name, int price) {
-        add(name, price, 1);
+        add(name, price, 1, 0);
     }
 
-    public void add(String name, int price, int count) {
+    public void add(String name, int price, int count) {  add(name, price, count, 0 ); }
+
+    public void add(String name, int price, int count, double weight){
         boolean error = false;
         if (contains(name)) {
             error = true;
@@ -50,18 +54,22 @@ public class Basket {
         }
 
         items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
+                count + " шт. - " + price + " - " + weight + " кг. ";
         totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + count * weight;
     }
 
     public void clear() {
         items = "";
         totalPrice = 0;
+        totalWeight = 0;
     }
 
     public int getTotalPrice() {
         return totalPrice;
     }
+
+    public double getTotalWeight() {return totalWeight;}
 
     public boolean contains(String name) {
         return items.contains(name);
@@ -73,6 +81,7 @@ public class Basket {
             System.out.println("Корзина пуста");
         } else {
             System.out.println(items);
+            System.out.println("Масса всех товаров в корзине: " + getTotalWeight());
         }
     }
 }
