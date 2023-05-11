5abdeba42da5996ba5422ac9a7afa2861174ea77
@ivan_okhlopkov <iokhlopkov@gmail.com>
2023-05-11 22:10:49 +0300

    fix Integer -> int

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,11 +1,13 @@
 public class Container {
-    private Integer count = 0;
+    private int count;
 
     public void addCount(int value) {
         count = count + value;
+
     }
 
     public int getCount() {
         return count;
     }
+
 }
